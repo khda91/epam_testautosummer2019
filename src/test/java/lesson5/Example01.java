ef99b31e6cda0bb9a79ced36cb06a3226548148c
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-21 20:36:38 +0300

    fixed project for jenkins

---
+++ src/test/java/lesson5/Example01.java
@@ -0,0 +1,31 @@
+package lesson5;
+
+import base.BaseTest;
+import lesson3.voids.HomePage;
+import lesson5.steps.ContactFormPageSteps;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Example01 extends BaseTest {
+
+    ContactFormPageSteps contactFormPageSteps;
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        contactFormPageSteps = new ContactFormPageSteps(driver);
+    }
+
+    @Test
+    public void simpleStepTest() {
+        HomePage hp = PageFactory.initElements(driver, HomePage.class);
+        hp.login("epam", "1234");
+
+        contactFormPageSteps.goToContactFormPage();
+        contactFormPageSteps.fillContactFormData("Male", "Some name");
+
+        // assert
+    }
+}
